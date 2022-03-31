737eb42208432f2c60bd5935ca060b9f344b8e27
Dana <danulja2005@inbox.lv>
2022-03-31 10:46:17 +0300

    js options

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('.js-auth-form')
 const errorButton = document.querySelector('.error_button_container button')
+const table = document.querySelector('.data-table table')
 
 if (form != null) {
     form.addEventListener('submit', (e) => {
@@ -17,6 +18,42 @@ if (form != null) {
         window.open("data.html", "_self")
     })
 }
+
+if (table != null) {
+    // ...
+}
+
+//var header = document.querySelector('table');
+//var section = document.querySelector('td');
+
+//var requestURL = 'vielas.json';
+//var request = new XMLHttpRequest();
+//request.open('GET', requestURL);
+//request.responseType = 'json';
+//request.send();
+//let json = JSON.stringify(value, [replacer, space])
+
+
+function readJSON(file, callback) {
+    let rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+ 
+}
+ 
+readJSON("vielas.json", function(text){
+    let data = JSON.parse(text);
+    console.log(vielas);
+});
+
+
+
 
 errorButton.addEventListener('click', () => {
     alert('Paldies, Jūsu ziņojums par kļūdu ir saņemts!')
