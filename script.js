a4c7dc2a82cab95a76402beb963db072c9b87cba
Dana <danulja2005@inbox.lv>
2022-04-21 12:34:51 +0300

    final version

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-/*common*/ 
+/*common*/
 const errorButton = document.querySelector('.error_button_container button')
 errorButton.addEventListener('click', () => {
-    alert('Paldies, Jūsu ziņojums par kļūdu ir saņemts!')
+    let issue_text = ''
+    do {
+        issue_text = prompt('Lūdzu, aprakstiet Jūsu kļūdu!')
+    } while (issue_text === '')
+    if (issue_text !== null) {
+        let request_body = JSON.stringify({
+            "title": "Kļūda no lapas \"" + document.title + "\"",
+            "body": issue_text,
+            "labels": ["bug"]
+        })
+        let xhr = new XMLHttpRequest()
+        xhr.open("POST", "https://api.github.com/repos/Dana-Oborenko/chemistry-system/issues")
+        xhr.setRequestHeader("Accept", "application/vnd.github.v3+json")
+        xhr.setRequestHeader("Content-Type", "application/json")
+        xhr.setRequestHeader("Authorization", "token " + "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")
+        xhr.addEventListener("readystatechange", function() {
+            if (this.readyState === 4) {
+                alert('Paldies, Jūsu ziņojums par kļūdu tika nosūtīts!')
+            }
+        })
+        xhr.send(request_body)
+    }
 })
 /*login*/
 if (document.title == 'Pieslēgšanās') {
@@ -30,43 +51,77 @@ if (document.title == 'Publiskie dati') {
     for (const cell of table.previousElementSibling.rows[0].cells) {
         keys.push(cell.textContent)
     }
-    console.log(keys)
-    
+
     const fetchJSONFiles = async () => {
         const VIELAS_DATA = await fetch('https://raw.githubusercontent.com/Dana-Oborenko/chemistry-system/main/data/vielas.json')
         const VIELAS = await VIELAS_DATA.json()
         
-        const APRIKOJUMS_DATA = await fetch('https://raw.githubusercontent.com/Dana-Oborenko/chemistry-system/main/data/inventars.json')
-        const APRIKOJUMS = await APRIKOJUMS_DATA.json()
+        const APRIKOJUMI_DATA = await fetch('https://raw.githubusercontent.com/Dana-Oborenko/chemistry-system/main/data/inventars.json')
+        const APRIKOJUMI = await APRIKOJUMI_DATA.json()
         
-        console.log(VIELAS);
-        console.log(APRIKOJUMS);
+        // console.log(VIELAS);
+        // console.log(APRIKOJUMI);
     
         VIELAS.forEach((viela) => {
             let newRow = document.createElement('tr')
-            Object.values(viela).forEach((value) => {
-                for (const key of keys) {
-                    let newCell = document.createElement('td')
-                    if (key == 'ID') {
-                        newCell.textContent = value['id']
-                    }
-                    if (key == 'Nosaukums') {
-                        newCell.textContent = value['nosaukums']                    
-                    }
-                    /* ... */
-                    newRow.appendChild(newCell)
+            for (const cell of table.previousElementSibling.rows[0].cells) {
+                let newCell = document.createElement('td')
+                switch (cell.textContent) {
+                    case 'ID':
+                        newCell.textContent = viela['id']
+                        break
+                    case 'Nosaukums':
+                        newCell.textContent = viela['nosaukums']
+                        break
+                    case 'Tips':
+                        newCell.textContent = viela['tips']
+                        break
+                    case 'Apakštips':
+                        newCell.textContent = viela['apakstips']
+                        break
+                    case 'Skaits':
+                        newCell.textContent = viela['skaits']
+                        break
+                    case 'Svars':
+                        newCell.textContent = viela['daudzums'] + ' ' + viela['mervienibas']
+                        break
+                    case 'Komentāri':
+                        newCell.textContent = viela['komentari']
+                        break
                 }
-            })
+                newRow.appendChild(newCell)
+            }
             table.appendChild(newRow)
         })
 
-        APRIKOJUMS.forEach((aprikojums) => {
+        APRIKOJUMI.forEach((aprikojums) => {
             let newRow = document.createElement('tr')
-            Object.values(aprikojums).forEach((value) => {
+            for (const cell of table.previousElementSibling.rows[0].cells) {
                 let newCell = document.createElement('td')
-                newCell.textContent = value
+                switch (cell.textContent) {
+                    case 'ID':
+                        newCell.textContent = aprikojums['id']
+                        break
+                    case 'Nosaukums':
+                        newCell.textContent = aprikojums['nosaukums']
+                        break
+                    case 'Tips':
+                        newCell.textContent = aprikojums['tips']
+                        break
+                    case 'Apakštips':
+                        newCell.textContent = aprikojums['apakstips']
+                        break
+                    case 'Skaits':
+                        newCell.textContent = aprikojums['skaits']
+                        break
+                    case 'Svars':
+                        break
+                    case 'Komentāri':
+                        newCell.textContent = aprikojums['komentari']
+                        break
+                }
                 newRow.appendChild(newCell)
-            })
+            }
             table.appendChild(newRow)
         })
 
@@ -80,102 +135,66 @@ if (document.title == 'Publiskie dati') {
             }
             array.push(obj)
         }
-        console.log(array)
-    }
-    fetchJSONFiles()
-
-    
-    
-    function refreshTable(result) {
-        table.innerHTML = ''
-        if (result.length > 0) {
-            result.forEach((obj) => {
-                let newRow = document.createElement('tr')
-                Object.values(obj).forEach((value) => {
-                    let newCell = document.createElement('td')
-                    newCell.textContent = value
-                    newRow.appendChild(newCell)
+        // console.log(array)
+        function refreshTable(result) {
+            table.innerHTML = ''
+            if (result.length > 0) {
+                result.forEach((obj) => {
+                    let newRow = document.createElement('tr')
+                    Object.values(obj).forEach((value) => {
+                        let newCell = document.createElement('td')
+                        newCell.textContent = value
+                        newRow.appendChild(newCell)
+                    })
+                    table.appendChild(newRow)
                 })
-                table.appendChild(newRow)
-            })
-        } else {
-            table.innerHTML = EMPTY_TABLE_CONTENTS
+            } else {
+                table.innerHTML = EMPTY_TABLE_CONTENTS
+            }
         }
-    }
-    
-    const searchInput = document.getElementById('search_input')
-    searchInput.addEventListener('keyup', function(event) {
-        if (event.code === 'Enter') {
-            searchButton.click()
-        }
-    })
-
-    const searchButton = document.getElementById('search_buttom')
-    searchButton.addEventListener('click', () => {
-        if (searchInput.value.length === 0) {
-            showAllButton.click()
-        } else {
-            const result = []
-            for (const obj of array) {
-                console.log(obj)
-                for (const [key, value] of Object.entries(obj)) {
-                    if (value.toUpperCase().includes(searchInput.value.toUpperCase())) {
-                        result.push(obj)
-                        break
+        const searchInput = document.getElementById('search_input')
+        searchInput.addEventListener('keyup', function(event) {
+            if (event.code === 'Enter') {
+                searchButton.click()
+            }
+        })
+        const searchButton = document.getElementById('search_buttom')
+        searchButton.addEventListener('click', () => {
+            if (searchInput.value === '') {
+                showAllButton.click()
+            } else {
+                const result = []
+                for (const obj of array) {
+                    // console.log(obj)
+                    for (const [key, value] of Object.entries(obj)) {
+                        if (value.toUpperCase().includes(searchInput.value.toUpperCase())) {
+                            result.push(obj)
+                            break
+                        }
                     }
                 }
+                refreshTable(result)
             }
+        })
+        const showAllButton = document.getElementById('show_all_button')
+        showAllButton.addEventListener('click', () => {
+            refreshTable(array)
+            searchInput.value = ''
+        })
+        const showMaterialsButton = document.getElementById('show_materials_button')
+        showMaterialsButton.addEventListener('click', () => {
+            const result = array.filter(obj => obj.Tips == 'Viela' || obj.Tips == 'reaģents')
+            // console.log(result)
             refreshTable(result)
-        }
-    })
-    const showAllButton = document.getElementById('show_all_button')
-    showAllButton.addEventListener('click', () => {
-        refreshTable(array)
-    })
-    const showMaterialsButton = document.getElementById('show_materials_button')
-    showMaterialsButton.addEventListener('click', () => {
-        const result = array.filter(obj => obj.Tips == 'Viela' || obj.Tips == 'reaģents')
-        console.log(result)
-        refreshTable(result)
-    })
-    const showToolsButton = document.getElementById('show_tools_button')
-    showToolsButton.addEventListener('click', () => {
-        const result = array.filter(obj => obj.Tips == 'Aprīkojums' || obj.Tips == 'svari' || obj.Tips == 'sildītājs' || obj.Tips == 'trauks')
-        console.log(result)
-        refreshTable(result)
-    })
-}
-
-//var header = document.querySelector('table');
-//var section = document.querySelector('td');
-
-//var requestURL = 'vielas.json';
-//var request = new XMLHttpRequest();
-//request.open('GET', requestURL);
-//request.responseType = 'json';
-//request.send();
-//let json = JSON.stringify(value, [replacer, space])
-
-
-/* function readJSON(file, callback) {
-    let rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
+            searchInput.value = ''
+        })
+        const showToolsButton = document.getElementById('show_tools_button')
+        showToolsButton.addEventListener('click', () => {
+            const result = array.filter(obj => obj.Tips == 'Aprīkojums' || obj.Tips == 'svari' || obj.Tips == 'sildītājs' || obj.Tips == 'trauks')
+            // console.log(result)
+            refreshTable(result)
+            searchInput.value = ''
+        })
     }
-    rawFile.send(null);
- 
-} */
-
-
-
-/*fetch('https://raw.githubusercontent.com/Dana-Oborenko/chemistry-system/main/data/inventars.json')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error));
-
-/*document.getElementById("name").innerHTML = JSONObject.Klients.Vārds + " " + JSONObject.Klients.Uzvārds
-document.getElementById("city").innerHTML = JSONObject["Piegādes adrese"].Pilsēta */
+    fetchJSONFiles().catch(error => console.log(error))
+}
